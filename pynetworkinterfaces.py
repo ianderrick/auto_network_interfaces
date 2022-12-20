f5905ece2a1ad6c5538711df36fb91122f56d67c
Ian Derrick <15677606+ianderrick@users.noreply.github.com>
2022-12-20 08:01:05 -0800

    Update pynetworkinterfaces.py

--- pynetworkinterfaces.py
+++ pynetworkinterfaces.py
@@ -3,35 +3,35 @@ import requests
 import getpass
 import datetime
 
-# check if the 'requests' module is installed
+# Check if the 'requests' module is installed
 try:
     import requests
 except ImportError:
-# if not, install it using pip
+# If not, install it using pip
     import pip
     pip.main(['install', 'requests'])
     import requests
 
-# get the hostname of the computer
+# Get the hostname of the computer
 hostname = socket.gethostname()
 
-# get the network interfaces of the computer
+# Get the network interfaces of the computer
 interfaces = socket.if_nameindex()
 
-# get the external IP address of the computer
+# Get the external IP address of the computer
 external_ip = requests.get("https://api.ipify.org").text
 
-# get the current user
+# Get the current user
 user = getpass.getuser()
 
-# get the current date and time
+# Get the current date and time
 date_time = datetime.datetime.now()
 
-# create a file to save the network information
+# Create a file to save the network information
 filename = "network_interfaces_" + hostname
 file = open(filename, "w")
 
-# write the current user and date and time to the file
+# Write the current user and date and time to the file
 file.write("Network information for user '%s' on %s\n" % (user, date_time))
 file.write("Hostname: %s\n" % hostname)
 file.write("External IP address: %s\n" % external_ip)
@@ -39,5 +39,5 @@ file.write("Network interfaces:\n")
 for interface in interfaces:
     file.write("%d: %s\n" % (interface[0], interface[1]))
 
-# close the file
+# Close the file
 file.close()
